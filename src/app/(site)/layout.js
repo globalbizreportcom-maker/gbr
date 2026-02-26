2fa43d69bb605ef599281f9ac0e5f7f7240e2da2
pabishek61001 <pabishek61001@gmail.com>
2026-02-26 17:39:07 +0530

    updated linkedin tag

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -171,11 +171,40 @@ fbq('track', 'PageView');`}
           <AlertProvider>
             {/* <TawkMessenger /> */}
 
-
             <Header />
             {children}
             <ScrollToTop />
             <Footer />
+
+            {/* LinkedIn Insight Tag */}
+            <Script id="linkedin-insight" strategy="afterInteractive">
+              {`
+    _linkedin_partner_id = "8860924";
+    window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+    window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+    (function(l) {
+      if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+      window.lintrk.q=[]}
+      var s = document.getElementsByTagName("script")[0];
+      var b = document.createElement("script");
+      b.type = "text/javascript"; b.async = true;
+      b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+      s.parentNode.insertBefore(b, s);
+    })(window.lintrk);
+  `}
+            </Script>
+
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: "none" }}
+                alt=""
+                src="https://px.ads.linkedin.com/collect/?pid=8860924&fmt=gif"
+              />
+            </noscript>
+
           </AlertProvider>
           {/* </CompanyProvider> */}
         </DashboardProvider>
