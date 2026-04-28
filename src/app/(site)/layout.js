510f4a6597bd8cb0145ac3a23ec31b9119c51cf6
pabishek61001 <pabishek61001@gmail.com>
2026-04-28 12:44:38 +0530

    test3

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -14,6 +14,7 @@ import TawkMessenger from "@/components/TawkMessenger";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  weight: ["400", "600", "700", "800", "900"], // Explicitly ask for 800
 });
 
 const geistMono = Geist_Mono({
